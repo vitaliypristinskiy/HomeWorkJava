174e75ede919adeef7a80fe70525526eb765381f
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-13 20:00:38 +0300

    init commit

---
+++ src/com/pb/pristinskiy/hw2/Calculator.java
@@ -0,0 +1,19 @@
+package com.pb.pristinskiy.hw2;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input number one");
+        int operand1 = scan.nextInt();
+
+        System.out.println("Input number two");
+        int operand2 = scan.nextInt();
+
+        System.out.println("Input operator");
+
+
+
+    }
+}
