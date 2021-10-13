b5d0518342b776e44bfe74669180586001eae8d3
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-13 21:13:30 +0300

    init commit

--- src/com/pb/pristinskiy/hw2/Calculator.java
+++ src/com/pb/pristinskiy/hw2/Calculator.java
@@ -1,17 +1,35 @@
 package com.pb.pristinskiy.hw2;
 import java.util.Scanner;
+import java.lang.String;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         System.out.println("Input number one");
-        int operand1 = scan.nextInt();
+        int number1 = scan.nextInt();
 
         System.out.println("Input number two");
-        int operand2 = scan.nextInt();
+        int number2 = scan.nextInt();
 
         System.out.println("Input operator");
+        String sing = scan.next();
+
+        switch (sing.charAt(0)) {
+            case '+':
+                System.out.println("number1 + number2 = " + (number1 + number2));
+                break;
+            case '-':
+                System.out.println("number1 - number2 = " + (number1 - number2));
+                break;
+            case '*':
+                System.out.println("number1 * number2 = " + (number1 * number2));
+                break;
+            case '/':
+                if (number2 != 0) {
+                    System.out.println("number1 / number2 = " + (number1 / number2));
+                } else { System.out.print("error, you cannot divide by zero");}
+                break;
+        }
 
 
 
