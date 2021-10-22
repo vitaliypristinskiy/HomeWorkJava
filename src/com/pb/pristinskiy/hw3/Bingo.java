ca2f8658bed1b8f841ffa43e7f41389dd875c8a8
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-22 20:56:03 +0300

    init commit

---
+++ src/com/pb/pristinskiy/hw3/Bingo.java
@@ -0,0 +1,35 @@
+package com.pb.pristinskiy.hw3;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int attempt = 0;
+        int Number= 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("ЗАДАЧА: Угадать число, Введите число от 1 до 100. Или число больше 100 для выхода.");
+
+        int MyNumber = random.nextInt(101);
+
+            do {
+                attempt++;
+                System.out.print("Введите ваше число: ");
+                Number = input.nextInt();
+
+                if (Number > 100) {
+                    break;
+                } else if (Number > MyNumber) {
+                    System.out.println("Моё число меньше.");
+                } else if (Number < MyNumber) {
+                    System.out.println("Моё число больше.");
+                } else if (Number == MyNumber) {
+                    System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
+                }
+            } while (Number != MyNumber);
+        System.out.println("Конец игры!");
+
+    }
+
+}
