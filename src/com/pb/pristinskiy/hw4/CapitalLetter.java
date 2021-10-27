0de056cc55652ca8dbea78c28b0298fe61a3fe4d
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-27 20:03:34 +0300

    init commit

--- src/com/pb/pristinskiy/hw4/CapitalLetter.java
+++ src/com/pb/pristinskiy/hw4/CapitalLetter.java
@@ -6,19 +6,21 @@ public class CapitalLetter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Пожайлуста введите текст: ");
+
         String source = scan.nextLine();
         StringBuilder res = new StringBuilder();
 
-        String[] strArr = source.split(" ");
+         String[] strArr = source.split(" ");
         for (String str : strArr) {
-            char[] stringArray = str.trim().toCharArray();
+           char[] stringArray = str.trim().toCharArray();
             stringArray[0] = Character.toUpperCase(stringArray[0]);
-            str = new String(stringArray);
+           str = new String(stringArray);
 
-            res.append(str).append(" ");
-        }
+           res.append(str).append(" ");
+         }
 
         System.out.print("Результат: " + res.toString().trim());
 
-    }
+
+   }
 }
