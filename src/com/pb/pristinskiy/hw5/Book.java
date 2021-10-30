dbfb4ac00f90c7965a9ba030691c8c06fac11487
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-30 17:49:20 +0300

    init commit

---
+++ src/com/pb/pristinskiy/hw5/Book.java
@@ -0,0 +1,31 @@
+package com.pb.pristinskiy.hw5;
+
+public class Book {
+    private String nameBook;
+    private String author;
+    private String year;
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
---
+++ src/com/pb/pristinskiy/hw5/Reader.java
@@ -0,0 +1,50 @@
+package com.pb.pristinskiy.hw5;
+
+public class Reader {
+    private String fullName;
+    private String readerNumber;
+    private String faculty;
+    private String dateBirth;
+    private String numberPhone;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getReaderNumber() {
+        return readerNumber;
+    }
+
+    public void setReaderNumber(String readerNumber) {
+        this.readerNumber = readerNumber;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(String dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+}
