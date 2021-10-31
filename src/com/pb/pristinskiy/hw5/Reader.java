8cac06e5a70bdc16fd8df6cbd7a5bd15dcf172f2
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-10-31 11:37:08 +0200

    init commit

--- src/com/pb/pristinskiy/hw5/Reader.java
+++ src/com/pb/pristinskiy/hw5/Reader.java
@@ -47,22 +47,71 @@ public class Reader {
         public void setNumberPhone (String numberPhone){
             this.numberPhone = numberPhone;
         }
-    public Reader(String fullName, String readerNumber, String faculty, String dateBirth, String numberPhone) {
+    public Reader(String fullName, String readerNumber, String faculty,
+                  String dateBirth, String numberPhone) {
         this.fullName = fullName;
         this.readerNumber = readerNumber;
         this.faculty = faculty;
         this.dateBirth = dateBirth;
         this.numberPhone = numberPhone;
     }
+    public void takeBook(int number) {
+        System.out.println(this.fullName + " взял " + number + " книги.");
+    }
+
+    public void takeBook(String... books) {
+        System.out.println(this.fullName + " взял следующие книги:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+    public void takeBook(Book... books) {
+        System.out.println(this.fullName + " взял следующие книги:");
+        for (Book book : books) {
+            System.out.println(book.getTitle() + ", автор - " + book.getAuthor());
+        }
+        System.out.println();
+    }
+
+    public void returnBook(int number) {
+        System.out.println(this.fullName + " вернул " + number + " книги.");
+    }
+
+    public void returnBook(String... books) {
+        System.out.println(this.fullName + " вернул следующие книги:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+    public void returnBook(Book... books) {
+        System.out.println(this.fullName + " вернул следующие книги:");
+        for (Book book : books) {
+            System.out.println(book.getTitle() + ", автор - " + book.getAuthor());
+        }
+        System.out.println();
+    }
+
+    public String getInfo() {
+        return "{" +
+                "ФИО='" + fullName + '\'' +
+                ", Номер читательского=" + readerNumber +
+                ", Факультет='" + faculty + '\'' +
+                ", Дата рождения='" + dateBirth + '\'' +
+                ", numberPhone='" + numberPhone + '\'' +
+                '}';
+    }
+
     public static void main(String[] args) {
         Book reader1 = new Book();
         Book reader2 = new Book();
         Book reader3 = new Book();
 
-        reader1.setTitle("Приключения");
-        System.out.println(reader1.getInfo());
-       // public void takeBook () {
+       // reader1.setTitle("Приключения");
+        //System.out.println(reader1.getInfo());
 
-       // }
     }
 }
