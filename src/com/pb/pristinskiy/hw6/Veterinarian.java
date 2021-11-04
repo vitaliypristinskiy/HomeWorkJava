c1dd9e3c6117764c974e2a7f94dcaf2d35b23031
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-11-04 21:50:25 +0200

    init commit

--- src/com/pb/pristinskiy/hw6/Veterinarian.java
+++ src/com/pb/pristinskiy/hw6/Veterinarian.java
@@ -2,6 +2,6 @@ package com.pb.pristinskiy.hw6;
 
 public class Veterinarian {
     public void treatAnimal(Animal animal) {
-        System.out.println(animal);
+        System.out.println(animal + "на  приеме у ветеринара");
     }
 }
