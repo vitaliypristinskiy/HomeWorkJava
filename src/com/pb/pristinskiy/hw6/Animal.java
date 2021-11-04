a2be762f7cfca33a1a4318dcd7e2068ac2be58c6
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-11-04 09:27:14 +0200

    init commit

---
+++ src/com/pb/pristinskiy/hw6/Animal.java
@@ -0,0 +1,22 @@
+package com.pb.pristinskiy.hw6;
+
+public class Animal {
+    private String food;
+    private String location;
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
