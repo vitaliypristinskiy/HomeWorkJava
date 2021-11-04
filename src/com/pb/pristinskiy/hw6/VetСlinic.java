a134e7ba18a2423f922f3d97e823f8ed17fa111d
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-11-04 21:45:52 +0200

    init commit

--- src/com/pb/pristinskiy/hw6/VetСlinic.java
+++ src/com/pb/pristinskiy/hw6/VetСlinic.java
@@ -1,15 +1,23 @@
 package com.pb.pristinskiy.hw6;
+import java.lang.reflect.Constructor;
 
 public class VetСlinic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Veterinarian veterinarian = new Veterinarian();
         Animal[] animals = new Animal[3];
         animals[0] = new Dog("Собачий корм", "Бутка");
         animals[1] = new Cat("Молоко и рыба", "Квартира многоэтажного дома");
         animals[2] = new Horse("Овес", "Конная ферма");
 
-        for (Animal animal : animals) {
-            veterinarian.treatAnimal(animal);
-        }
+
+
+        Class<?> vetClazz = Class.forName("com.pb.pristinskiy.hw6.Veterinarian");
+        Constructor constr = vetClazz.getConstructor();
+        Object obj = constr.newInstance();
+        if (obj instanceof Veterinarian) {
+            for (Animal animal : animals) {
+            ((Veterinarian) obj).treatAnimal(animal);
+            }
+       }
     }
 }
