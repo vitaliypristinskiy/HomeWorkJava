c77937fc0179dac86bc63767d1a7a677beab9d5f
Vitaliy Pristinskiy <deverionvp@gmail.com>
2021-11-14 20:16:07 +0200

    init commit

---
+++ src/com/pb/pristinskiy/hw7/Skirt.java
@@ -0,0 +1,21 @@
+package com.pb.pristinskiy.hw7;
+
+public class Skirt extends Clothes implements WomenClothes {
+    public Skirt(Size size, String color) {
+        super(size, color);
+    }
+
+    public Skirt(Size size, String color, double cost) {
+        super(size, color, cost);
+    }
+
+    @Override
+    public String toString() {
+        return "Юбка{" +
+                "размер = " + getSize() +
+                ", цена = " + getCost() + " грн" +
+                ", цвет = " + getColor() +
+                "}";
+    }
+
+}
